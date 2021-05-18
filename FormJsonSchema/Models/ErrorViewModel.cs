447e47ad8013625d6a0dd85dd7bc6120dca814c3
jaspreet singh <39166752+jaspreetchhibber@users.noreply.github.com>
2021-05-18 10:38:25 +0530

    First Commit

---
+++ FormJsonSchema/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FormJsonSchema.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
