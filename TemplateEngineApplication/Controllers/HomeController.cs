a77e2addb7a65736cb45efbcc1f37aee45e7758f
jaspreet singh <39166752+jaspreetchhibber@users.noreply.github.com>
2021-05-24 17:36:15 +0530

    Updated TEmplate

--- TemplateEngineApplication/Controllers/HomeController.cs
+++ TemplateEngineApplication/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using TemplateEngineApplication.Models;
@@ -12,10 +14,11 @@ namespace TemplateEngineApplication.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private IHostingEnvironment _environment;
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment environment)
         {
             _logger = logger;
+            _environment = environment;
         }
 
         public IActionResult Index()
@@ -32,6 +35,25 @@ namespace TemplateEngineApplication.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpGet]
+        public async Task<JsonResult> GetJsonFile()
+        {
+            string wwwPath = _environment.WebRootPath;
+            string path = Path.Combine(wwwPath, "jsonfiles");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string fileName = Path.GetFileName("schema.json");
+            string filePath = Path.Combine(path, fileName);
+
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                var file = r.ReadToEnd();
+                return Json(new { file });
+            }
+            return new JsonResult(null);
         }
     }
 }
